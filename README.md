1e1cb231612f4456281262bcca822ca397cd96f6
conceptree <nunomtrodrigues@gmail.com>
2023-07-24 22:55:04 +0100

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # nodejs-static-server-keycloak-protected
 A simple example of two apps serving and sharing static files between each other under keycloak authorization protection
+
+## USAGE
+After cloning this repo you will need to run `npm install` inside *app1* and *app2* to install each server dependencies.
+
+To run both apps simultaneous you can run `npm run start` from this repo root folder or just run the same command from each app folder individually.
---
+++ app1/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const Keycloak = require('keycloak-connect');
+
+const config = require("./keycloak.json");
+
+const app = express();
+
+/* // Keycloak configuration
+const keycloak = new Keycloak(config);
+
+// Middleware to protect the static files
+app.use(keycloak.middleware({
+  logout: '/logout', // Specify the logout URL
+})); */
+
+// Serve static files from the public folder
+app.use(express.static('public'));
+
+// Start the server
+const port = 4000; // You can use any port you prefer
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
